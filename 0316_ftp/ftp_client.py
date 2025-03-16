bd126e392fd9bfe6ed6b2141429482027b0f7890
33dk3942k7ji333 <52348257+33dk3942k7ji333@users.noreply.github.com>
2025-03-16 23:22:39 +0800

    Add files via upload

--- 0316_ftp/ftp_client.py
+++ 0316_ftp/ftp_client.py
@@ -3,13 +3,16 @@ import logging
 from pathlib import Path
 from collections import defaultdict
 
-logging.basicConfig(level=logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-
 
 class FTPClient:
-    def __init__(self, url: str, user: str, pwd: str):
+    def __init__(
+        self,
+        url: str,
+        user: str,
+        pwd: str,
+        logger: logging.Logger = None,
+    ):
+        self.logger = logger if logger is not None else logging.getLogger(__name__)
         self.ftp = ftplib.FTP(url)
         self.ftp.login(user, pwd)
         self.ftp_welcome = self.ftp.getwelcome()
@@ -17,7 +20,12 @@ class FTPClient:
 
     # region Download
     @staticmethod
-    def walk_ftp_dir(ftp: ftplib.FTP, dir_remote: str, recursive: bool = True) -> tuple[list[str], dict]:
+    def walk_ftp_dir(
+        ftp: ftplib.FTP,
+        dir_remote: str,
+        logger: logging.Logger,
+        recursive: bool = True,
+    ) -> tuple[list[str], dict]:
         lst_file = list()
         dic_dir = defaultdict(list)
         cnt_depth = 0
@@ -51,93 +59,103 @@ class FTPClient:
         recursive_list_dir(ftp, dir_remote, recursive)
         return lst_file, dict(dic_dir)
 
-    def download_ftp_file(self, dir_remote: str, dir_local: str, recursive: bool = True):
+    def download_ftp_file(self, path_remote: str, path_local: str):
+        try:
+            path_file = Path(path_local)
+            path_file.parent.mkdir(parents=True, exist_ok=True)
+            self.logger.info(f"Download File [{path_remote}] to [{path_file}]")
+            with open(path_file, "wb") as f:
+                self.ftp.retrbinary(f"RETR {path_remote}", f.write)
+        except Exception as e:
+            self.logger.error(f"Error: {e}", exc_info=True)
+
+    def download_ftp_files(self, dir_remote: str, dir_local: str, recursive: bool = True):
         self.ftp.cwd("/")
         # Get File List
-        logger.info(f"Walking Remote Directory [{dir_remote}] with recursive is {recursive}")
-        lst_file, dic_dir = self.walk_ftp_dir(self.ftp, dir_remote, recursive)
-        logger.info(f"Found Directory: {dic_dir}")
-        logger.info(f"Get File List: {lst_file}")
+        self.logger.info(f"Walking Remote Directory [{dir_remote}] with recursive is {recursive}")
+        lst_file, dic_dir = self.walk_ftp_dir(self.ftp, dir_remote, self.logger, recursive)
+        self.logger.info(f"Found Directory: {dic_dir}")
+        self.logger.info(f"Get File List: {lst_file}")
 
         # Download File
-        logger.info("Start Download Files in File List")
+        self.logger.info("Start Download Files in File List")
         cnt_downloaded = 0
         for idx, file in enumerate(lst_file):
             _batch = f"[{idx + 1:2d}/{len(lst_file):2d}]"
             try:
                 path_file = Path(dir_local) / file.lstrip(dir_remote).lstrip("/")
                 path_file.parent.mkdir(parents=True, exist_ok=True)
-                logger.info(f"{_batch} Download File [{file}] to [{path_file}]")
+                self.logger.info(f"{_batch} Download File [{file}] to [{path_file}]")
                 with open(path_file, "wb") as f:
                     self.ftp.retrbinary(f"RETR {file}", f.write)
                 cnt_downloaded += 1
             except Exception as e:
-                logger.error(f"{_batch} Error : {e}", exc_info=True)
+                self.logger.error(f"{_batch} Error : {e}", exc_info=True)
 
         self.ftp.cwd("/")
-        logger.info(f"Download Task Finished: {cnt_downloaded}/{len(lst_file)}")
+        self.logger.info(f"Download Task Finished: {cnt_downloaded}/{len(lst_file)}")
 
     # endregion Download
 
     # region Upload
-    def upload_local_file(self, dir_upload: str, dir_remote: str):
+    def upload_local_files(self, dir_upload: str, dir_remote: str):
         self.ftp.cwd("/")
-        logger.info(f"Start Upload Files from [{dir_upload}]")
+        self.logger.info(f"Start Upload Files from [{dir_upload}]")
         cnt_depth = 0
         cnt_uploaded = 0
 
         def walk_upload_dir(dir_cur: str, dir_cur_remote: str):
             nonlocal cnt_depth, cnt_uploaded
-            logger.info(f"Upload Object in [{dir_cur}] to [{dir_cur_remote}]")
+            self.logger.info(f"Upload Object in [{dir_cur}] to [{dir_cur_remote}]")
 
             try:
                 self.ftp.cwd(dir_cur_remote)
-                logger.info(f"Change FTP Directory to [{dir_cur_remote}]")
+                self.logger.info(f"Change FTP Directory to [{dir_cur_remote}]")
             except ftplib.error_perm:
                 self.ftp.mkd(dir_cur_remote)
                 self.ftp.cwd(dir_cur_remote)
-                logger.info(f"Create and Change FTP Directory [{dir_cur_remote}]")
+                self.logger.info(f"Create and Change FTP Directory [{dir_cur_remote}]")
             except Exception as e:
-                logger.error(f"Change FTP Directory to [{dir_cur_remote}] Failed: {e}", exc_info=True)
+                self.logger.error(f"Change FTP Directory to [{dir_cur_remote}] Failed: {e}", exc_info=True)
 
             for obj in Path(dir_cur).glob("*"):
                 if obj.is_dir():
-                    logger.info(f"[{cnt_depth}] Object {obj} is Directory")
+                    self.logger.info(f"[{cnt_depth}] Object {obj} is Directory")
                     cnt_depth += 1
                     walk_upload_dir(obj, obj.name)
                     self.ftp.cwd("..")
                 else:
                     remote_file = Path(self.ftp.pwd()) / obj.name
-                    logger.info(f"[{cnt_depth}] Upload: {obj} to {remote_file}")
+                    self.logger.info(f"[{cnt_depth}] Upload: {obj} to {remote_file}")
                     try:
                         with open(obj, "rb") as f:
                             self.ftp.storbinary(f"STOR {obj.name}", f)
                         cnt_uploaded += 1
                     except Exception as e:
-                        logger.error(f"Upload Error: {e}", exc_info=True)
+                        self.logger.error(f"Upload Error: {e}", exc_info=True)
             cnt_depth -= 1
 
         walk_upload_dir(dir_upload, dir_remote)
         self.ftp.cwd("/")
-        logger.info(f"Uploaded {cnt_uploaded} Files")
+        self.logger.info(f"Uploaded {cnt_uploaded} Files")
 
     # endregion Upload
 
     def disconnect(self):
         if hasattr(self, "ftp"):
             try:
-                logger.debug(f"FTPClient {id(self)} Disconnecting...")
+                self.logger.debug(f"FTPClient {id(self)} Disconnecting...")
                 self.ftp.quit()
             except ftplib.all_errors as e:
-                logger.error(f"FTP Error: {e}", exc_info=True)
+                self.logger.error(f"FTP Error: {e}", exc_info=True)
             except Exception as e:
-                logger.error(f"Error: {e}", exc_info=True)
+                self.logger.error(f"Error: {e}", exc_info=True)
             finally:
                 self.ftp = None
 
     def __del__(self):
         self.disconnect()
-        logger.debug(f"FTPClient {id(self)} Exit")
+        self.logger.debug(f"FTPClient {id(self)} Exit")
 
 
 if __name__ == "__main__":
@@ -150,6 +168,12 @@ if __name__ == "__main__":
     DIR_LOCAL = "jobId123/res/tool_file"
     DIR_UPLOAD = "jobId123/res/to_upload"
 
-    ftp_client = FTPClient(FTP_URL, FTP_USER, FTP_PWD)
-    ftp_client.download_ftp_file(DIR_REMOTE, DIR_LOCAL)
-    ftp_client.upload_local_file(DIR_UPLOAD, DIR_REMOTE_UPLOAD)
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    ftp_client = FTPClient(FTP_URL, FTP_USER, FTP_PWD, logger)
+    ftp_client.download_ftp_files(DIR_REMOTE, DIR_LOCAL)
+    ftp_client.upload_local_files(DIR_UPLOAD, DIR_REMOTE_UPLOAD)
+    
+    PATH_REMOTE = "CDSC/dummy1/dummy2/time.txt"
+    PATH_LOCAL = "job_single/job123/time123.txt"
+    ftp_client.download_ftp_file(PATH_REMOTE, PATH_LOCAL)
